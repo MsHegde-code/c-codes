552bf103e74e218135b529f8245a64cda9e017ed
manitshegde <manitshegde@gmail.com>
2021-06-11 23:12:07 +0530

    first commit

---
+++ math.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float s,p,d,q,a,b; 
+	printf("enter any two numbers");
+	scanf("%f%f",&a,&b);
+	s=a+b;
+	p=a*b;
+	d=a-b;
+	q=a/b;
+	printf("sum is :%f\nproduct is :%f\ndifference is :%f\nquotient is :%f\n",s,p,d,q);
+	printf("\n \n \nCODE DEVELOPED BY MANIT\n");
+return 0;}
