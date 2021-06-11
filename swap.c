883ac65b491fb1bb5f529570b72a876edd907363
manitshegde <84580747+manitshegde@users.noreply.github.com>
2021-06-11 23:30:28 +0530

    swap.c
    swapping 2 numbers in the output screen without third variable..

---
+++ swap.c
@@ -0,0 +1,8 @@
+/* WRITE A PROGRAM TO SWAP TWO NUMBERS IN THE OUTPUT SCREEN WITHOUT THIRD VARIABLE, BY ACCEPTING THE NUMBERS BY THE USER */
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("enter two numbers to be swapped\n");
+	scanf("%d%d",&a,&b);
+	printf("AFTER SWAPPING \n%d\n%d\n",b,a);
+return 0;}
