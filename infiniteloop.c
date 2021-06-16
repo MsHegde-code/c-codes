4357c37391d40922283646b620041f0841fd551f
manitshegde <84580747+manitshegde@users.noreply.github.com>
2021-06-16 21:46:21 +0530

    small bug fix

--- infiniteloop.c
+++ infiniteloop.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main(){
     int i=1,n=2;
-    printf("this is an infinite loop");
     while(n>=1)
     {
-        printf("%d is a loop",n);
+        printf("this is a loop");
         i++;
     }
     return 0;
