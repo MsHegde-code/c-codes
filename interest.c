dda1ba4b248f49eb1fe6a50a2ad3f1e161f33231
manitshegde <84580747+manitshegde@users.noreply.github.com>
2021-06-16 15:38:33 +0530

    Update interest.c
    interest rate should be in percentage, so divide i by 100

--- interest.c
+++ interest.c
@@ -3,7 +3,7 @@ int main(){
     float i,p,t,r;
     printf("enter the values for 'p','t' and 'r'\n");
     scanf("%f%f%f",&p,&t,&r);
-    i=p*r*t;
+    i=(p*r*t)/100;
     printf("the interest is %f",i);
     return 0;
 }
