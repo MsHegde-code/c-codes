2b45af6892b10e7ba1ddb20e5749cba3f82be258
manitshegde <84580747+manitshegde@users.noreply.github.com>
2021-06-16 21:43:38 +0530

    small update

--- infiniteloop.c
+++ infiniteloop.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 int main(){
-    int i=1,n;
-    printf("enter a number greater than 1 to begin the infinite loop");
-    scanf("%d",&n);
+    int i=1,n=2;
+    printf("this is an infinite loop");
     while(n>=1)
     {
         printf("%d is a loop",n);
