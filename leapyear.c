81c323c09439902cfd21df920de95761df840e76
manitshegde <84580747+manitshegde@users.noreply.github.com>
2021-06-21 11:47:56 +0530

    leap year update, every 100 years - leap year is skipped..

--- leapyear.c
+++ leapyear.c
@@ -6,7 +6,7 @@ int main(){
     if((y%4)==0){
         printf("It is a leap year\n");
     }
-    else if((y%100)==0){
+    else if((y%400)==0){
         printf("It is a leap year\n");
     }
     else if(y<0){
