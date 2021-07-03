3c3b4a9efdee340020268902e2a26ba177ac624e
manit.git <84580747+manitshegde@users.noreply.github.com>
2021-07-03 19:54:49 +0530

    star pattern 2
    new star pattern

---
+++ starpattern2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    printf("star pattern 1\n");
+    for(j=5;j>=1;j--){// to print new lines
+    for(i=1;i<=5;i++){
+        if((i+j)<=5)  //when i+j is <= 5
+            printf(" ");    //print space
+        else
+            printf("*");    //print star
+    }
+    printf("\n");
+    }
+    return 0;
+}
