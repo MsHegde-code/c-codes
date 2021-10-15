2baa4e76acffb48962ba2cd3ea1d83dc72a91dae
MSH <84580747+manitshegde@users.noreply.github.com>
2021-10-15 18:25:10 +0530

    new

--- swap.c
+++ swap.c
@@ -1,8 +1,13 @@
-/* WRITE A PROGRAM TO SWAP TWO NUMBERS IN THE OUTPUT SCREEN WITHOUT THIRD VARIABLE, BY ACCEPTING THE NUMBERS BY THE USER */
 #include<stdio.h>
-int main(){
-	int a,b;
-	printf("enter two numbers to be swapped\n");
-	scanf("%d%d",&a,&b);
-	printf("AFTER SWAPPING \n%d\n%d\n",b,a);
-return 0;}
+void main(){
+    int variable_1,variable_2;
+    printf("enter the numbers\n");
+    scanf("%d%d",&variable_1,&variable_2);
+    printf("before swapping\n");
+    printf("%d\t%d",variable_1,variable_2);
+    variable_1=variable_1+variable_2;
+    variable_2=variable_1-variable_2;
+    variable_1=variable_1-variable_2;
+    printf("\nafter swapping\n %d\t%d",variable_1,variable_2);
+
+}
